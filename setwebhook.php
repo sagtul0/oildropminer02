d96179901f57f190bc2b17c3c10d315d724a62a4
sag tul <sagtul0@gmail.com>
2025-03-09 02:21:58 +0330

    اصلاح setwebhook.php با توکن و URL درست

--- setwebhook.php
+++ setwebhook.php
@@ -1,6 +1,6 @@
 <?php
 $bot_token = 'YOUR_BOT_TOKEN'; // توکن باتت رو بذار
-$webhook_url = 'https://oildropminer.onrender.com/telegram.php'; // URL سرورت
+$webhook_url = 'https://oildropminer02-eay2.onrender.com/telegram.php'; // URL سرورت
 
 $api_url = "https://api.telegram.org/bot$bot_token/setWebhook?url=" . urlencode($webhook_url);
 $response = file_get_contents($api_url);
