7cd7a09daf0f3af92f30cadc5589f45942d3d3bf
sag tul <sagtul0@gmail.com>
2025-03-15 11:44:36 +0330

    Re-added test-db.php to ensure it's included

--- test_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $db = new PDO("pgsql:host=" . parse_url(getenv("DATABASE_URL"))["host"] . ";port=" . parse_url(getenv("DATABASE_URL"))["port"] . ";dbname=" . ltrim(parse_url(getenv("DATABASE_URL"))["path"], "/") . ";user=" . parse_url(getenv("DATABASE_URL"))["user"] . ";password=" . parse_url(getenv("DATABASE_URL"))["pass"]);
-    echo "اتصال موفق!";
-} catch (PDOException $e) {
-    echo "خطا: " . $e->getMessage();
-}
-?>
